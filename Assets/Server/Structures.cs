2134a6dc01054992ab1e3dcc42fb5b14d7119499
Per Åkerström <akerstrom.perry@gmail.com>
2021-04-24 20:16:06 +0200

    Game controller stuff

---
+++ Assets/Server/Structures.cs
@@ -0,0 +1,49 @@
+﻿using Netkraft.Messaging;
+/// <summary>
+///data structures and stuff
+/// </summary>
+
+namespace GameStructure
+{
+    [System.Serializable]
+    public struct TimeLine : IWritable
+    {
+        public Block[] timeLine;
+    }
+
+    [System.Serializable]
+    public struct Block : IWritable
+    {
+        public ushort level;
+        public Attribute[] mods;
+        public Character[] characters;
+        public int[] branches;
+    }
+
+    [System.Serializable]
+    public struct Character : IWritable
+    {
+        [SkipIndex] public int Id;
+        [SkipIndex] public int GridId;
+        public string name;
+        public byte color;
+        public byte role;
+        public Attribute[] attributes;
+        public InputFrame[] timeLine;
+    }
+
+    [System.Serializable]
+    public struct Attribute : IWritable
+    {
+        public byte type;
+        public ushort value;
+    }
+
+    [System.Serializable]
+    public struct InputFrame : IWritable
+    {
+        public byte action;
+        public ushort cell;
+    }
+}
+
