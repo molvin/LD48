a003e4fcfcc6c4fcf746c13ddf5151d552ce2f2a
DanielAkesson <argilstudio@gmail.com>
2021-04-24 22:12:41 +0200

    fixed merge conflic

--- Assets/Server/Structures.cs
+++ Assets/Server/Structures.cs
@@ -1,5 +1,4 @@
 ﻿using Netkraft.Messaging;
-<<<<<<< HEAD
 //data structures and stuff
 namespace GameStructure
 {
@@ -33,61 +32,14 @@ namespace GameStructure
     }
     [Writable]
     public struct LDAttribute
-=======
-/// <summary>
-///data structures and stuff
-/// </summary>
-
-namespace GameStructure
-{
-    [System.Serializable]
-    public struct TimeLine : IWritable
-    {
-        public Block[] timeLine;
-    }
-
-    [System.Serializable]
-    public struct Block : IWritable
-    {
-        public ushort level;
-        public Attribute[] mods;
-        public Character[] characters;
-        public int[] branches;
-    }
-
-    [System.Serializable]
-    public struct Character : IWritable
-    {
-        [SkipIndex] public int Id;
-        [SkipIndex] public int GridId;
-        public string name;
-        public byte color;
-        public byte role;
-        public Attribute[] attributes;
-        public InputFrame[] timeLine;
-    }
-
-    [System.Serializable]
-    public struct Attribute : IWritable
->>>>>>> 2f9d67a796f61b4f6d4faba62faa79f6b72bcb65
     {
         public byte type;
         public ushort value;
     }
-<<<<<<< HEAD
     [Writable]
     public struct LDInputFrame
-=======
-
-    [System.Serializable]
-    public struct InputFrame : IWritable
->>>>>>> 2f9d67a796f61b4f6d4faba62faa79f6b72bcb65
     {
         public byte action;
         public ushort cell;
     }
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 2f9d67a796f61b4f6d4faba62faa79f6b72bcb65
