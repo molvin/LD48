e3cd0f88fcfecc2bda08ec0adc919763560f5e41
Daniel Fors <daniel.lennart.fors@gmail.com>
2021-04-25 22:25:35 +0200

    Enemies

---
+++ Assets/AbilitySystem/Game/Tags/NoActionTag.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameplayAbilitySystem;
+
+[CreateAssetMenu(menuName = "Ability System/Tag/No Action")]
+public class NoActionTag : TypeTag { }
