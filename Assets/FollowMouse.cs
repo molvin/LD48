636ebe10c3ab78aad0300bc99de7252e07538741
DanielAkesson <argilstudio@gmail.com>
2021-04-25 20:57:36 +0200

    fixed

--- Assets/FollowMouse.cs
+++ Assets/FollowMouse.cs
@@ -8,22 +8,30 @@ using static GridGenerator;
 public class FollowMouse : MonoBehaviour
 {
     public GridGenerator gridGen;
+    public GameObject selectionGrid;
     public Action<Vector3Int> CellSelected;
     public System.Type AbilityType;
     // Update is called once per frame
 
+    private void OnEnable()
+    {
+        selectionGrid.SetActive(true);
+    }
+    private void OnDisable()
+    {
+        selectionGrid.SetActive(false);
+    }
     private void Awake()
     {
         gameObject.SetActive(false);
     }
-
     void Update()
     {
         
         Vector3 world = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3Int iso = gridGen.WorldToCell(new Vector3(world.x, 0, world.z));
         iso.x = Mathf.Clamp(iso.x, 0, gridGen.xWidth);
-        iso.y = Mathf.Clamp(iso.x, 0, gridGen.zWidth);
+        iso.y = Mathf.Clamp(iso.y, 0, gridGen.zWidth);
         bool can_cast = GameStateManager.Instance.PlayerAgent.AbilitySystem.CanActivateTargetAbilityByTag(AbilityType, (Vector2Int)iso);
 
         if(!can_cast)
