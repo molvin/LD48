910f45aca437d56cc89f5d7977476764aed39566
Mans Becker <Mans.Becker@CM.lan>
2021-04-26 17:26:41 +0200

    tags paralyzing, smokebomb, magicshrom

---
+++ Assets/AbilitySystem/Game/Tags/BlindTag.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameplayAbilitySystem;
+
+[CreateAssetMenu(menuName = "Ability System/Tag/BlindTag")]
+public class BlindTag : TypeTag { }
