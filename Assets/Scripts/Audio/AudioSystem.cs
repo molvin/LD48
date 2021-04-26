524393c9d148b6b3ef2dcd5f43266e0e44b940ef
Daniel Fors <daniel.lennart.fors@gmail.com>
2021-04-26 22:35:22 +0200

    audio

--- Assets/Scripts/Audio/AudioSystem.cs
+++ Assets/Scripts/Audio/AudioSystem.cs
@@ -3,23 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-[CreateAssetMenu]
-public class AudioObject : ScriptableObject
-{
-    public AudioClip[] Clips;
-    public AudioMixerGroup MixerGroup;
-    public float MinVolume = 1;
-    public float MaxVolume = 1;
-    public float MinPitch = 1;
-    public float MaxPitch = 1;
-    public bool Loop = false;
-    public float SpatialBlend = 0;
-
-    public AudioClip GetClip()
-    {
-        return Clips.Length == 0 ? null : Clips[Random.Range(0, Clips.Length)];
-    }
-}
 
 public class AudioSystem
 {
